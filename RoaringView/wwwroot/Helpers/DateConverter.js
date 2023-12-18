b79554d68281952965541b09fa16e3f4b9eaa11b
Przemek0799 <przemyslaw.walkowski@student.hv.se>
2023-12-18 11:08:29 +0100

    changed datepicker and fixed dateformat

--- RoaringView/wwwroot/Helpers/DateConverter.js
+++ RoaringView/wwwroot/Helpers/DateConverter.js
@@ -1,6 +1,12 @@
-﻿function initializeDatePickers() {
-    $('.datepicker-input').datepicker({
-        format: 'dd/mm/yyyy',
-        autoclose: true
+﻿function initializeFlatpickr(elementId, dotNetReference, methodName) {
+    flatpickr(document.getElementById(elementId), {
+        dateFormat: "d/m/Y",
+        onChange: function (selectedDates, dateStr, instance) {
+            dotNetReference.invokeMethodAsync(methodName, dateStr);
+        },
+        onReady: function (dateObj, dateStr, instance) {
+            instance.input.classList.add("form-control");
+            instance.input.style.backgroundColor = "#fff"; // Set white background
+        }
     });
 }
